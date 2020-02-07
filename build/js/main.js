c6196734674754103d427763939e44ee23dd4a04
Max Grachikov <max.grachikov@yandex.ru>
2020-02-07 15:38:54 +0300

    Пофиксил баг мобильного меню

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-$(".header-wrapper__burger").click(function(){$(".header-wrapper__burger,.menu").toggleClass("active")});
+$(".header-wrapper__burger").click(function(){$(".header-wrapper__burger,.menu").toggleClass("active")}),$(".header-wrapper__burger").on("click",function(){"relative"==$("header").css("position")?$("header").css("position","static"):$("header").css("position","relative")});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 $(".header-wrapper__burger").click(function() {
 	$(".header-wrapper__burger,.menu").toggleClass("active");
 });
+
+// Фикс меню для мобил
+$(".header-wrapper__burger").on("click", function() {
+	if($("header").css("position") == "relative") {
+		$("header").css("position", "static");
+	}
+	else {
+		$("header").css("position", "relative");
+	}
+
+})
