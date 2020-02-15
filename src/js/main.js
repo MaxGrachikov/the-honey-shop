c5724d17632d7f09a0dbbd2f89782937a9b822ea
Max Grachikov <max.grachikov@yandex.ru>
2020-02-15 14:59:53 +0300

    Доработал функционал модальных окон в блоке 'Продукция'

--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,23 @@ $(".header-wrapper__burger").click(function() {
 });
 
 $(".product__button-buy").click(function() {
-	$(".modal-wrapper").css("display", "flex");
+	$(".modal").css("display", "flex");
 	$(".feedback-form").css("display", "flex");
 	$("body").css("overflow","hidden");
 	$(".feedback-form__button-close-form").click(function() {
 		$(".feedback-form").css("display", "none");
-		$(".modal-wrapper").css("display", "none");
+		$(".modal").css("display", "none");
 		$("body").css("overflow","");
 	})
 });
 
 $(".product__button-more-detailed").click(function() {
-	$(".modal-wrapper").css("display", "flex");
+	$(".modal").css("display", "flex");
 	$(".about-on-product").css("display", "flex");
 	$("body").css("overflow","hidden");
-	$(".about-on-product").click(function() {
+	$(".about-on-product__button-close").click(function() {
 		$(".about-on-product").css("display", "none");
-		$(".modal-wrapper").css("display", "none");
+		$(".modal").css("display", "none");
 		$("body").css("overflow","");
 	})
 });
